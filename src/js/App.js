da496872f77c4125baa16fe2a0584f4778c97e0c
nrkroeker <nathaliekroeker@gmail.com>
2017-06-13 21:22:43 -0400

    File structure updated

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Header from './Header'
-import Article from './Article'
-import Ad from './Ad'
-import AuxiliaryArticles from './AuxiliaryArticles'
-import Footer from './Footer'
+import Header from '../Header/Header'
+import Article from '../Article/Article'
+import Ad from '../Ad/Ad'
+import AuxiliaryArticles from '../AuxiliaryArticles/AuxiliaryArticles'
+import Footer from '../Footer/Footer'
 
 class App extends Component {
   render() {
